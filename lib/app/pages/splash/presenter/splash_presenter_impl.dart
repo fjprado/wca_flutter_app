92e6b6af51dc53076d0b3e57a22a07c4bfdabe64
Fernando José do Prado <fernando.prado@elastacloud.com>
2022-10-15 15:40:21 -0300

    Removed fake login and added initial logout

--- lib/app/pages/splash/presenter/splash_presenter_impl.dart
+++ lib/app/pages/splash/presenter/splash_presenter_impl.dart
@@ -1,3 +1,4 @@
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wca_flutter_app/app/pages/splash/view/splash_view.dart';
 
 import './splash_presenter.dart';
@@ -8,8 +9,11 @@ class SplashPresenterImpl implements SplashPresenter {
   @override
   Future<void> checkLogin() async {
     _view.showLoader();
-    await Future.delayed(Duration(seconds: 2));
-    _view.logged(false);
+    
+    final sp = await SharedPreferences.getInstance();
+    final accessToken = sp.getString('accessToken');
+
+    _view.logged(accessToken != null);
   }
 
   @override
